78eb77967c4786fa156ba11cba587a8389e88daf
Jendnice <Pipeline38@hotmail.com>
2020-01-03 15:07:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = batteryBatches.reduce(function(total, element){ return element + total}, 0)
+
+
+
+// let doubledAndSummedFromTen = [1, 2, 3].reduce(function(total, element){ return element * 2 + total}, 10)
